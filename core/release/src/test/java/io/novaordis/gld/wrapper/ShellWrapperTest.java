6ed0e4fbfa03c87165c14a7e46ffa343a602e7b9
Ovidiu Feodorov <ovidiu@novaordis.com>
2017-01-11 12:29:11 -0500

    first successful bash unit test

--- core/release/src/test/java/io/novaordis/gld/wrapper/ShellWrapperTest.java
+++ core/release/src/test/java/io/novaordis/gld/wrapper/ShellWrapperTest.java
@@ -16,9 +16,20 @@
 
 package io.novaordis.gld.wrapper;
 
+import io.novaordis.utilities.Files;
+import io.novaordis.utilities.os.NativeExecutionException;
+import io.novaordis.utilities.os.NativeExecutionResult;
+import io.novaordis.utilities.os.OS;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.File;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 /**
  * @author Ovidiu Feodorov <ovidiu@novaordis.com>
@@ -32,19 +43,94 @@ public class ShellWrapperTest {
 
     // Static ----------------------------------------------------------------------------------------------------------
 
+    protected static File scratchDirectory;
+
+    private static File getShlibFile() throws Exception {
+
+        String basedir = System.getProperty("basedir");
+        File d = new File(basedir);
+        assertTrue(d.isDirectory());
+
+        File f = new File(d, "src/main/bash/gld.shlib");
+        assertTrue(f.isFile());
+        return f;
+    }
+
+    /**
+     * @throws NativeExecutionException if the function execution fails with a non-zero exit code. The exception's
+     * message contains the stderr content.
+     */
+    private static String executeShellFunction(String functionName, String... args)  throws Exception {
+
+        File shlib = getShlibFile();
+
+        String wrapperContent =
+                "#/bin/bash\n\n" +
+                        ". " + shlib.getPath() + "\n\n" +
+                        functionName + " ";
+
+        for (String arg : args) {
+
+            wrapperContent += arg + " ";
+        }
+
+        wrapperContent += "\n\n";
+
+        File wrapperFile = new File(scratchDirectory, "test.sh");
+        assertTrue(Files.write(wrapperFile, wrapperContent));
+        assertTrue(Files.chmod(wrapperFile, "r-xr-xr-x"));
+
+        NativeExecutionResult r = OS.getInstance().execute(scratchDirectory, "bash ./test.sh");
+
+        if (r.isFailure()) {
+
+            throw new NativeExecutionException(r.getStderr());
+
+        }
+        return r.getStdout();
+    }
+
     // Attributes ------------------------------------------------------------------------------------------------------
 
     // Constructors ----------------------------------------------------------------------------------------------------
 
     // Public ----------------------------------------------------------------------------------------------------------
 
+    @Before
+    public void before() throws Exception {
+
+        String projectBaseDirName = System.getProperty("basedir");
+        scratchDirectory = new File(projectBaseDirName, "target/test-scratch");
+        assertTrue(scratchDirectory.isDirectory());
+    }
+
+    @After
+    public void after() throws Exception {
+
+        //
+        // scratch directory cleanup
+        //
+
+        assertTrue(io.novaordis.utilities.Files.rmdir(scratchDirectory, false));
+    }
+
     // Tests -----------------------------------------------------------------------------------------------------------
 
+    // get-extension-jars ----------------------------------------------------------------------------------------------
+
     @Test
-    public void test() throws Exception {
+    public void getExtensionJars_NoExtensionDirectory() throws Exception {
 
-        log.info("test");
+        try {
+            executeShellFunction("get-extension-jars");
+            fail("should have thrown exception");
+        }
+        catch(NativeExecutionException e) {
 
+            String msg = e.getMessage();
+            log.info(msg);
+            assertTrue(msg.startsWith("extension directory not provided"));
+        }
     }
 
     // Package protected -----------------------------------------------------------------------------------------------
